d013548bfbd8be962d8ca4795fcf0612ad1642bb
Gabby <gabbyvbyers@gmail.com>
2026-03-31 20:44:39 -0700

    Abbreviated 'Violet' as 'Vi'. Pulled out the previous contents of main.cpp into a header-only 'Demo.h' module.

--- main.cpp
+++ main.cpp
@@ -6,8 +6,28 @@
 #include "Window.h"
 #include "Demo.h"
 
+class Planet {
+public:
+	
+};
+
+class Map {
+
+};
+
 int main() {
-	Vi::demo_application();
+	Vi::Window window("AHHHHHHH", 1920, 1080);
+	Vi::Camera camera;
+
+	while (window.is_open()) {
+		window.poll_events();
+		window.clear(Vi::Color::blue() * 0.2f);
+		
+
+
+		window.display();
+	}
+
 	return 0;
 }
 
